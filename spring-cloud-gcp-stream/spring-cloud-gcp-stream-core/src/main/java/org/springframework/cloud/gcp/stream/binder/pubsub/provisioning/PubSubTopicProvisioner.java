ad5d18285b62524842f618713816f0cffc007441
João André Martins <joaomartins@google.com>
2017-08-11 10:46:22 -0400

    Adds Spring Cloud Stream GCP Pub/Sub binder
    Fixes #85

---
+++ spring-cloud-gcp-stream/spring-cloud-gcp-stream-core/src/main/java/org/springframework/cloud/gcp/stream/binder/pubsub/provisioning/PubSubTopicProvisioner.java
@@ -0,0 +1,55 @@
+/*
+ *  Copyright 2017 original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.springframework.cloud.gcp.stream.binder.pubsub.provisioning;
+
+import org.springframework.cloud.gcp.pubsub.PubSubAdmin;
+import org.springframework.cloud.gcp.stream.binder.pubsub.properties.PubSubConsumerProperties;
+import org.springframework.cloud.gcp.stream.binder.pubsub.properties.PubSubProducerProperties;
+import org.springframework.cloud.stream.binder.ExtendedConsumerProperties;
+import org.springframework.cloud.stream.binder.ExtendedProducerProperties;
+import org.springframework.cloud.stream.provisioning.ConsumerDestination;
+import org.springframework.cloud.stream.provisioning.ProducerDestination;
+import org.springframework.cloud.stream.provisioning.ProvisioningException;
+import org.springframework.cloud.stream.provisioning.ProvisioningProvider;
+
+/**
+ * @author João André Martins
+ */
+public class PubSubTopicProvisioner implements ProvisioningProvider<
+		ExtendedConsumerProperties<PubSubConsumerProperties>,
+		ExtendedProducerProperties<PubSubProducerProperties>> {
+
+	private final PubSubAdmin pubSubAdmin;
+
+	public PubSubTopicProvisioner(PubSubAdmin pubSubAdmin) {
+		this.pubSubAdmin = pubSubAdmin;
+	}
+
+	@Override
+	public ProducerDestination provisionProducerDestination(String name,
+			ExtendedProducerProperties<PubSubProducerProperties> properties)
+			throws ProvisioningException {
+		return new PubSubProducerDestination(name);
+	}
+
+	@Override
+	public ConsumerDestination provisionConsumerDestination(String name, String group,
+			ExtendedConsumerProperties<PubSubConsumerProperties> properties)
+			throws ProvisioningException {
+		return new PubSubConsumerDestination(name);
+	}
+}
